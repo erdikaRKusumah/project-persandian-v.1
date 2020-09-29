4dc6baaebd211c0982bafbc3538bdc1c4bc365ae
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2020-09-29 10:00:56 +0700

    lupa commit untuk fitur capctha

--- app/Http/Controllers/KelolaController.php
+++ app/Http/Controllers/KelolaController.php
@@ -43,7 +43,7 @@ class KelolaController extends Controller
         
         $pilihan = json_encode($request->pilihan);
         
-        
+        //aaaa
 
         // $id_kategori = DB::table('kategori')->select('id_kategori')->where('kategori', $request->id_kategori)->get();
        
@@ -75,9 +75,13 @@ class KelolaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Request $request, $id = null)
     {
-        //
+        if($request->isMethod('post')) {
+            $data = $request->all();
+            Pertanyaan::where(['id'=>$id])->update(['id'=>$data['id'], 'id_kategori'=>$data['id_kategori'], 'pilihan'=>$data['pilihan'], 'pertanyaan'=>$data['pertanyaan']]);
+            return redirect()->back();
+        }
     }
 
     /**
@@ -100,6 +104,7 @@ class KelolaController extends Controller
      */
     public function destroy($id)
     {
-        return $id_pertanyaan;
+        DB::table('pertanyaans')->where('id',$id)->delete();
+        return redirect('/kelolaPertanyaan');
     }
 }
