35ccadaef6b3a42ff548c831f58ef156ea0affec
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2020-10-21 12:03:25 +0700

    Menambahkan form wizard di halaman kuesioner

--- resources/views/client/test.blade.php
+++ resources/views/client/test.blade.php
@@ -1,5 +1,53 @@
 @extends('layouts.client')
+@section('css')
+<style type="text/css">
+/* Mark input boxes that gets an error on validation: */
 
+/* Hide all steps by default: */
+.tab {
+  display: none;
+}
+
+button {
+  background-color: #4CAF50;
+  color: #ffffff;
+  border: none;
+  padding: 10px 20px;
+  font-size: 17px;
+  font-family: Raleway;
+  cursor: pointer;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+#prevBtn {
+  background-color: #bbbbbb;
+}
+
+/* Make circles that indicate the steps of the form: */
+.step {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbbbbb;
+  border: none;  
+  border-radius: 50%;
+  display: inline-block;
+  opacity: 0.5;
+}
+
+.step.active {
+  opacity: 1;
+}
+
+/* Mark the steps that are finished and valid: */
+.step.finish {
+  background-color: #4CAF50;
+}
+</style>
+@endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -26,9 +74,14 @@
                         </div>
                     @endif
 
-                    <form method="POST" action="{{ route('client.test.store') }}">
+                    <form method="POST" action="{{ route('client.test.store') }}" id="regForm">
                         @csrf
+                        <div class="tab">
                         @foreach($categories as $category)
+                        
+                        
+                        @if($category->id == '6')
+                        
                             <div class="card mb-3">
                                 <div class="card-header">{{ $category->name }}</div>
                 
@@ -58,13 +111,108 @@
                                     @endforeach
                                 </div>
                             </div>
-                        @endforeach
+                            @endif
+                            @endforeach
+                            </div>
 
-                        <div class="form-group row mb-0">
+                            <div class="tab">
+                        @foreach($categories as $category)
+                        
+                        
+                        @if($category->id == '7')
+                        
+                            <div class="card mb-3">
+                                <div class="card-header">{{ $category->name }}</div>
+                
+                                <div class="card-body">
+                                    @foreach($category->categoryQuestions as $question)
+                                        <div class="card @if(!$loop->last)mb-3 @endif">
+                                            <div class="card-header">{{ $question->question_text }}</div>
+                        
+                                            <div class="card-body">
+                                                <input type="hidden" name="questions[{{ $question->id }}]" value="">
+                                                @foreach($question->questionOptions as $option)
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="questions[{{ $question->id }}]" id="option-{{ $option->id }}" value="{{ $option->id }}"@if(old("questions.$question->id") == $option->id) checked @endif>
+                                                        <label class="form-check-label" for="option-{{ $option->id }}">
+                                                            {{ $option->option_text }}
+                                                        </label>
+                                                    </div>
+                                                @endforeach
+
+                                                @if($errors->has("questions.$question->id"))
+                                                    <span style="margin-top: .25rem; font-size: 80%; color: #e3342f;" role="alert">
+                                                        <strong>{{ $errors->first("questions.$question->id") }}</strong>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                            @endif
+                            @endforeach
+                            </div>
+
+                            <div class="tab">
+                        @foreach($categories as $category)
+                        
+                        
+                        @if($category->id == '8')
+                        
+                            <div class="card mb-3">
+                                <div class="card-header">{{ $category->name }}</div>
+                
+                                <div class="card-body">
+                                    @foreach($category->categoryQuestions as $question)
+                                        <div class="card @if(!$loop->last)mb-3 @endif">
+                                            <div class="card-header">{{ $question->question_text }}</div>
+                        
+                                            <div class="card-body">
+                                                <input type="hidden" name="questions[{{ $question->id }}]" value="">
+                                                @foreach($question->questionOptions as $option)
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="questions[{{ $question->id }}]" id="option-{{ $option->id }}" value="{{ $option->id }}"@if(old("questions.$question->id") == $option->id) checked @endif>
+                                                        <label class="form-check-label" for="option-{{ $option->id }}">
+                                                            {{ $option->option_text }}
+                                                        </label>
+                                                    </div>
+                                                @endforeach
+
+                                                @if($errors->has("questions.$question->id"))
+                                                    <span style="margin-top: .25rem; font-size: 80%; color: #e3342f;" role="alert">
+                                                        <strong>{{ $errors->first("questions.$question->id") }}</strong>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                            @endif
+                            @endforeach
+                            </div>
+                            
+                        
+                        <div style="overflow:auto;">
+  <div style="float:right;">
+    <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
+    <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
+    <button type="submit" id="BtnSubmit">Submit</button>
+  </div>
+</div>
+
+<!-- Circles which indicates the steps of the form: -->
+<div style="text-align:center;margin-top:40px;">
+  <span class="step"></span>
+  <span class="step"></span>
+  <span class="step"></span>
+</div>
+                        <!-- <div class="form-group row mb-0">
                             <div class="col-md-6">
                                 <button type="submit" class="btn btn-primary">
                                     Submit
-                                </button>
+                                </button> -->
                             </div>
                         </div>
                     </form>
@@ -73,4 +221,79 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+<script type="text/javascript"> 
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1) ) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+    document.getElementById("BtnSubmit").style.display = "inline";
+    document.getElementById("nextBtn").style.display = "none";
+   
+    
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+    document.getElementById("BtnSubmit").style.display = "none";
+  }
+  //... and run a function that will display the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+} 
+
+</script>
 @endsection
