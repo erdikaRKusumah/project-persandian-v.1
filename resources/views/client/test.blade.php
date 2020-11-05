24f982ffaf3a7d3611023d68edb894718bbd0027
erdikaRKusumah <erdikar.kusumah@mail.unpas.ac.id>
2020-11-05 19:14:42 +0700

    menambahkan kategori kuesioner

--- resources/views/client/test.blade.php
+++ resources/views/client/test.blade.php
@@ -192,6 +192,162 @@ button:hover {
                             @endif
                             @endforeach
                             </div>
+
+                            <div class="tab">
+                                @foreach($categories as $category)
+                                
+                                
+                                @if($category->id == '9')
+                                
+                                    <div class="card mb-3">
+                                        <div class="card-header">{{ $category->name }}</div>
+                        
+                                        <div class="card-body">
+                                            @foreach($category->categoryQuestions as $question)
+                                                <div class="card @if(!$loop->last)mb-3 @endif">
+                                                    <div class="card-header">{{$loop->iteration}}. {{ $question->question_text }}</div>
+                                
+                                                    <div class="card-body">
+                                                        <input type="hidden" name="questions[{{ $question->id }}]" value="">
+                                                        @foreach($question->questionOptions as $option)
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="radio" name="questions[{{ $question->id }}]" id="option-{{ $option->id }}" value="{{ $option->id }}"@if(old("questions.$question->id") == $option->id) checked @endif>
+                                                                <label class="form-check-label" for="option-{{ $option->id }}">
+                                                                    {{ $option->option_text }}
+                                                                </label>
+                                                            </div>
+                                                        @endforeach
+        
+                                                        @if($errors->has("questions.$question->id"))
+                                                            <span style="margin-top: .25rem; font-size: 80%; color: #e3342f;" role="alert">
+                                                                <strong>{{ $errors->first("questions.$question->id") }}</strong>
+                                                            </span>
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                    @endif
+                                    @endforeach
+                                    </div>
+
+                                    <div class="tab">
+                                        @foreach($categories as $category)
+                                        
+                                        
+                                        @if($category->id == '10')
+                                        
+                                            <div class="card mb-3">
+                                                <div class="card-header">{{ $category->name }}</div>
+                                
+                                                <div class="card-body">
+                                                    @foreach($category->categoryQuestions as $question)
+                                                        <div class="card @if(!$loop->last)mb-3 @endif">
+                                                            <div class="card-header">{{$loop->iteration}}. {{ $question->question_text }}</div>
+                                        
+                                                            <div class="card-body">
+                                                                <input type="hidden" name="questions[{{ $question->id }}]" value="">
+                                                                @foreach($question->questionOptions as $option)
+                                                                    <div class="form-check">
+                                                                        <input class="form-check-input" type="radio" name="questions[{{ $question->id }}]" id="option-{{ $option->id }}" value="{{ $option->id }}"@if(old("questions.$question->id") == $option->id) checked @endif>
+                                                                        <label class="form-check-label" for="option-{{ $option->id }}">
+                                                                            {{ $option->option_text }}
+                                                                        </label>
+                                                                    </div>
+                                                                @endforeach
+                
+                                                                @if($errors->has("questions.$question->id"))
+                                                                    <span style="margin-top: .25rem; font-size: 80%; color: #e3342f;" role="alert">
+                                                                        <strong>{{ $errors->first("questions.$question->id") }}</strong>
+                                                                    </span>
+                                                                @endif
+                                                            </div>
+                                                        </div>
+                                                    @endforeach
+                                                </div>
+                                            </div>
+                                            @endif
+                                            @endforeach
+                                            </div>
+
+                                            <div class="tab">
+                                                @foreach($categories as $category)
+                                                
+                                                
+                                                @if($category->id == '11')
+                                                
+                                                    <div class="card mb-3">
+                                                        <div class="card-header">{{ $category->name }}</div>
+                                        
+                                                        <div class="card-body">
+                                                            @foreach($category->categoryQuestions as $question)
+                                                                <div class="card @if(!$loop->last)mb-3 @endif">
+                                                                    <div class="card-header">{{$loop->iteration}}. {{ $question->question_text }}</div>
+                                                
+                                                                    <div class="card-body">
+                                                                        <input type="hidden" name="questions[{{ $question->id }}]" value="">
+                                                                        @foreach($question->questionOptions as $option)
+                                                                            <div class="form-check">
+                                                                                <input class="form-check-input" type="radio" name="questions[{{ $question->id }}]" id="option-{{ $option->id }}" value="{{ $option->id }}"@if(old("questions.$question->id") == $option->id) checked @endif>
+                                                                                <label class="form-check-label" for="option-{{ $option->id }}">
+                                                                                    {{ $option->option_text }}
+                                                                                </label>
+                                                                            </div>
+                                                                        @endforeach
+                        
+                                                                        @if($errors->has("questions.$question->id"))
+                                                                            <span style="margin-top: .25rem; font-size: 80%; color: #e3342f;" role="alert">
+                                                                                <strong>{{ $errors->first("questions.$question->id") }}</strong>
+                                                                            </span>
+                                                                        @endif
+                                                                    </div>
+                                                                </div>
+                                                            @endforeach
+                                                        </div>
+                                                    </div>
+                                                    @endif
+                                                    @endforeach
+                                                    </div>
+
+                                                    <div class="tab">
+                                                        @foreach($categories as $category)
+                                                        
+                                                        
+                                                        @if($category->id == '12')
+                                                        
+                                                            <div class="card mb-3">
+                                                                <div class="card-header">{{ $category->name }}</div>
+                                                
+                                                                <div class="card-body">
+                                                                    @foreach($category->categoryQuestions as $question)
+                                                                        <div class="card @if(!$loop->last)mb-3 @endif">
+                                                                            <div class="card-header">{{$loop->iteration}}. {{ $question->question_text }}</div>
+                                                        
+                                                                            <div class="card-body">
+                                                                                <input type="hidden" name="questions[{{ $question->id }}]" value="">
+                                                                                @foreach($question->questionOptions as $option)
+                                                                                    <div class="form-check">
+                                                                                        <input class="form-check-input" type="radio" name="questions[{{ $question->id }}]" id="option-{{ $option->id }}" value="{{ $option->id }}"@if(old("questions.$question->id") == $option->id) checked @endif>
+                                                                                        <label class="form-check-label" for="option-{{ $option->id }}">
+                                                                                            {{ $option->option_text }}
+                                                                                        </label>
+                                                                                    </div>
+                                                                                @endforeach
+                                
+                                                                                @if($errors->has("questions.$question->id"))
+                                                                                    <span style="margin-top: .25rem; font-size: 80%; color: #e3342f;" role="alert">
+                                                                                        <strong>{{ $errors->first("questions.$question->id") }}</strong>
+                                                                                    </span>
+                                                                                @endif
+                                                                            </div>
+                                                                        </div>
+                                                                    @endforeach
+                                                                </div>
+                                                            </div>
+                                                            @endif
+                                                            @endforeach
+                                                            </div>
                             
                         
                         <div style="overflow:auto;">
@@ -204,6 +360,10 @@ button:hover {
 
 <!-- Circles which indicates the steps of the form: -->
 <div style="text-align:center;margin-top:40px;">
+  <span class="step"></span>
+  <span class="step"></span>
+  <span class="step"></span>
+  <span class="step"></span>
   <span class="step"></span>
   <span class="step"></span>
   <span class="step"></span>
